291e47c00961828c967f83d25a81acd0e866fa09
Lorent lorch Bellonara otherys <lbellona@at-l7.21-school.ru>
2019-04-08 22:50:07 +0300

    read_from_file_done

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: lbellona <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/06 20:10:35 by lbellona          #+#    #+#             */
-/*   Updated: 2019/04/03 23:29:28 by lbellona         ###   ########.fr       */
+/*   Updated: 2019/04/08 22:45:21 by lbellona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,9 +70,9 @@ void				clean_img(t_img_params *img)
 			img->data[i] = 0;
 }
 
-void				*pr_error(void)
+void				*pr_error(char *err_msg)
 {
-					perror("Error");
+					perror(err_msg);
 					exit(0);
 }
 
@@ -115,31 +115,117 @@ void				draw_landscape(t_3d_coords *coords)
 	mlx_loop(win.mlx_ptr);
 }
 
-int					read_map(int fd, t_3d_coords *coords)
+t_3d_coords			*ft_create_lst_elem(int x, int y, int z)
+{
+	t_3d_coords		*new_elem;
+
+	if ((new_elem = (t_3d_coords*)malloc(sizeof(t_3d_coords))))
+	{
+		new_elem->next = 0;
+		new_elem->x = x;
+		new_elem->y = y;
+		new_elem->z = z;
+	}
+	else
+		pr_error("memory allocation error");
+	return (new_elem);
+}
+
+void            	ft_list_push_back(t_3d_coords **begin_list, t_3d_coords *cur_elem)
+{
+	t_3d_coords 	*tmpptr;
+
+	if (!(*begin_list))
+		*begin_list = cur_elem;
+	else
+	{
+		tmpptr = *begin_list;
+		while (tmpptr->next)
+			tmpptr = tmpptr->next;
+		tmpptr->next = cur_elem;
+	}
+}
+
+void				ft_print_lst(t_3d_coords *coords)
+{
+	while (coords)
+	{
+		printf("%d ", coords->z);
+		coords = coords->next;
+	}
+}
+
+int					add_coords_2_lst(t_3d_coords **coords,
+									char *line, t_3d_size *map_size, int delta)
+{
+	t_3d_coords     *cur_elem;
+
+	if (!(cur_elem = ft_create_lst_elem(map_size->width - 1,
+								map_size->height - 1, ft_atoi(line + delta))))
+	{
+		//free allocated memery
+		return (0);
+	}
+	ft_list_push_back(coords, cur_elem);
+	return (1);
+}
+
+int					get_3d_coords(char *line, t_3d_coords **coords,
+															t_3d_size *map_size)
+{
+	int				i;
+	int				delta;
+
+	i = -1;
+	delta = 0;
+	map_size->width = 0;
+	while (line[++i])
+	{
+
+		if ((line[i] >= '0' && line[i] <='9') || line[i] == '-')
+		{
+			delta = i;
+			map_size->width++;
+			while ((line[i] >= '0' && line[i] <='9') || line[i] == '-')
+				i++;
+			if (!line[i])
+				i--;
+			else
+				line[i] = 0;
+			if (!(add_coords_2_lst(coords, line, map_size, delta)))
+				return (0);
+			printf("%d ", ft_atoi(line + delta));
+		}
+	}
+	return (1);
+}
+
+t_3d_coords			*read_map(int fd, t_3d_size *map_size)
 {
 	char 			*line;
-	int				height;
-	int				width;
 	int				i;
+	t_3d_coords		*coords;
 
 	i = 0;
-	counter = 0;
+	coords = 0;
+	map_size->height = 0;
 	while ((i = get_next_line(fd, &line)) > 0)
 	{
-		height++;
-		//i = get_next_line(fd, &line);
-		//printf("%d\n", i);
-		printf("line = %s\n", line);
+		map_size->height++;
+		if(!(get_3d_coords(line, &coords, map_size)))
+			return (0);
+		printf("\n");
 	}
-	printf("%d\n", height);
-
-	return (1);
+	printf("\n");
+	close(fd);
+	return (coords);
 }
 
 int					main(int argc, char **argv)
 {
 	int				fd;
-	t_3d_coords		coords;
+	t_3d_coords		*coords;
+	t_3d_size		map_size;
 
 	errno = 0;
 	if (argc != 3)
@@ -147,13 +233,15 @@ int					main(int argc, char **argv)
 	else
 	{
 		if ((fd = open(argv[1], O_RDONLY)) < 0)
-			return ((int)pr_error());
-		if (read_map(fd, &coords))
+			return ((int)pr_error(""));
+		if ((coords = read_map(fd, &map_size)))
 		{
+			ft_print_lst(coords);
+			//printf("%d\n", map_size.height);
 			//draw_landscape(&coords);
 		}
 		else
-			return ((int)pr_error());
+			return ((int)pr_error("map error"));
 	}
 
 	return (0);
