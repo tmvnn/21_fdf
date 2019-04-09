77f839a1261f01c9ae241cf7f2aa64d3053ef430
Lorent lorch Bellonara otherys <lbellona@at-k5.21-school.ru>
2019-04-09 22:49:32 +0300

    gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbellona <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/15 15:22:41 by lbellona          #+#    #+#             */
+/*   Updated: 2018/12/24 17:58:25 by lbellona         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+
+# include "libft/libft.h"
+
+# define SF_ALCN(ptr) if (!ptr) return (-1);
+
+int				get_next_line(const int fd, char **line);
+
+#endif
